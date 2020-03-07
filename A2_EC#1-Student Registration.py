fb5c54e84bebe4fe210a35d2e6c94d2b5f35e131
mike <mikea62@gmail.com>
2020-03-07 13:06:15 -0800

    Second commit

---
+++ A2_EC#1-Student Registration.py
@@ -0,0 +1,14 @@
+name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+year_born = input("Enter the year you were born: ")
+
+print("Registration Form")
+print()
+print("First name: ", name)
+print("Last name: ", last_name)
+print("Birth date: ", year_born)
+print()
+print("Welcome ", name, last_name,"!")
+print("Your registration is complete.")
+print("Your temporary password is: ", name + "*" + year_born)
+
